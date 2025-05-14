7283a581b5314b0b045c0c4faf1ac301b15a93f3
yosuad <yosuad@hotmail.com>
2025-05-14 11:00:57 -0500

    avance_nosotrosandinicio

--- js/emisora.js
+++ js/emisora.js
@@ -2,7 +2,7 @@ const audioPlayer = document.getElementById("audioPlayer");
 const audio = document.getElementById("audio");
 const icono = document.getElementById("icono");
 const loader = document.querySelector(".request-loader");
-
+const volumeControl = document.getElementById('volumeControl');
 
 
 let isPlaying = false;
@@ -15,7 +15,7 @@ audioPlayer.addEventListener("click", () => {
     // Remplazar boton de Play por stop
     icono.classList.replace("fa-stop", "fa-play");
     icono.classList.add("ms-1");
-    // Remplazar boton de Play por stop   
+    // Remplazar boton de Play por stop
       //    AGREGAR EFECTO
       loader.classList.add("efecto");
   } else {
@@ -30,3 +30,34 @@ audioPlayer.addEventListener("click", () => {
   }
   isPlaying = !isPlaying;
 });
+
+
+
+var controlBox = document.getElementsByClassName("control-box")[0];
+var volumeContainer = controlBox.querySelector(".volume-control");
+
+function setBars() {
+  const volume = volumeControl.value
+  console.log(volume);
+  
+  audio.volume = volume;
+	
+	volumeContainer.className = "volume-control";
+
+	if (volume > 0) {
+    console.log("subir");
+    
+		controlBox.classList.add("volume-on");
+    console.log("volume-" + volume*100);
+    
+		volumeContainer.classList.add("volume-" + (volume*100));
+	} else {
+    console.log("bajar");
+    
+		controlBox.classList.remove("volume-on");
+	}
+}
+
+
+volumeControl.addEventListener("input", setBars);
+setBars();
